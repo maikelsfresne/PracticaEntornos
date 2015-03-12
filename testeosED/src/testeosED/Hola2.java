c6b9d83e64d76f9419f0a221835785b271286ef9
maikelsfresne <maikelsvivaundertaker@hotmail.com>
2015-03-12 13:49:11 +0100

    holaaaaaaaa

--- testeosED/src/testeosED/Hola2.java
+++ testeosED/src/testeosED/Hola2.java
@@ -4,5 +4,6 @@ public class Hola2 {
 	public static void main(String[] args) {
 		System.out.println("Clase 2 de prueba");
 		System.out.println("Adiós");
+		System.out.println("Hola de nuevo");
 	}
 }
